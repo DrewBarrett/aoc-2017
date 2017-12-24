27d866b05e22d2a79379ca005a9ef798028cf3ae
Drew Barrett <dfb1998@gmail.com>
2017-12-24 13:20:16 -0500

    day3 p1

--- day3/day3.py
+++ day3/day3.py
@@ -18,25 +18,36 @@ def distance(input):
     width = nearest
     height = nearest
     current = nearest * nearest
+    side = 3
     while current != input:
+        mod = 0
         current -= 1
-        if width > 1:
-            x -= 1
-            width -= 1
-        elif height > 1 :
-            y -= 1
-            height -= 1
+        if side == 3 or side == 2:
+            mod = -1
         else:
-            x += 1
+            mod = 1
+        if width > 1:
+            x += mod
+            width -= 1
+            if width == 1:
+                side -= 1
+                height = nearest
+        elif height > 1:
+            y += mod
+            height -= 1
+            if height == 1:
+                side -= 1
+                width = nearest
     return abs(x) + abs(y)
 
 
 assert nearestOddSquare(1) == 1
 assert nearestOddSquare(8) == 3
-#assert distance(1) == 0
-#assert distance(12) == 3
-#assert distance(23) == 2
-#assert distance(1024) == 31
+assert distance(1) == 0
+assert distance(12) == 3
+assert distance(23) == 2
+assert distance(1024) == 31
 
 for line in file:
-    print(nearestOddSquare(int(line)))
+    print(distance(int(line)))
+
