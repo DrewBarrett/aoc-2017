f4af04783b7992a7688de0b8c86f916f4e00e7d5
Drew Barrett <dfb1998@gmail.com>
2017-12-24 12:03:12 -0500

    day4 p2

--- day4/main.py
+++ day4/main.py
@@ -10,17 +10,36 @@ def checkDup(arg1):
         else:
             return True
     return False
-    
+
+def checkAna(arg1):
+    words = arg1.split(" ")
+    for word in words:
+        for word2 in words:
+            if word2 != word and len(word2) == len(word):
+                ana = True
+                for letter in word2:
+                    if word2.count(letter) != word.count(letter):
+                        ana = False
+                if ana:
+                    return True
+    return False
 
 assert checkDup("aa bb cc dd ee")==False
 assert checkDup("aa bb cc dd aa")==True
 assert checkDup("aa bb cc dd aaa")==False
-
+assert checkAna("abcde fghij")==False
+assert checkAna("abcde xyz ecdab")==True
+assert checkAna("a ab abc abd abf abj")==False
+assert checkAna("iiii oiii ooii oooi oooo")==False
+assert checkAna("oiii ioii iioi iiio")==True
 
 valid = 0
-
+valid2 = 0
 for line in file:
     if not checkDup(line.strip()):
         valid += 1
+    if not checkDup(line.strip()) and not checkAna(line.strip()):
+        valid2 += 1
 
 print(valid)
+print(valid2)
